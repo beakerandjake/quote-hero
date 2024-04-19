76dd4c35da0bb3bf0a83c5f89549b4a57963887c
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-19 11:52:23 -0700

    better output and piping to log file

--- ingest/ingest.sh
+++ ingest/ingest.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# fail if any command fails.
+set -euo pipefail
+# write all output to log file
+log_file=ingest.log
+exec 3>&1 1>"$log_file" 2>&1
+
 date=20240415
 url=https://dumps.wikimedia.org/other/cirrussearch/${date}/enwikiquote-${date}-cirrussearch-content.json.gz
 raw_file=wikiquote_raw.json.gz
@@ -7,13 +13,13 @@ raw_file=wikiquote_raw.json.gz
 
 # download the elasticsearch dump file from wikimedia.
 if [ -e $raw_file ]; then
-    echo 'wikiquote dump file already exists, skipping download'
+    echo 'Skipping wikiquote dump download, file already exists.' >&3
 else 
-    echo "downloading wikiquote dump file"
+    echo 'Downloading wikiquote dump file.' >&3
     wget $url -O $raw_file
     # if wget did not return 200 then bail
     if [ $? -ne 0 ]; then
-        echo 'failed to download dump file'
+        echo "Failed to download dump file, see $log_file" >&3
         # clean up empty file that wget wrote.
         rm $raw_file
         exit 1
@@ -23,45 +29,53 @@ fi
 
 # split the large dump file into many smaller files.
 # during the split pipe each line through our custom formatter which removes data we won't be using. 
-echo 'splitting dump file into smaller chunks'
-mkdir -p chunks
-gunzip -c $raw_file | split -d --filter='python3 format_bulk_data.py > $FILE' -l 500 - ./chunks/wikiquote_chunk_
-
+chunk_count=0
+if [ -d chunks ]; then
+    chunk_count=$(ls chunks -1q | wc -l)
+    echo 'Not splitting dump file, chunk directory exists.' >&3
+else 
+    echo 'Splitting dump file into smaller chunks.' >&3
+    mkdir -p chunks
+    gunzip -c $raw_file | split -d --filter='python3 format_bulk_data.py > $FILE' -l 500 - ./chunks/wikiquote_chunk_
+    chunk_count=$(ls chunks -1q | wc -l)
+    echo "Split file into $chunk_count chunk(s)." >&3
+fi
 
 # create the elastic index we will load the data into
-echo 'creating elasticsearch index' 
-curl --fail-with-body --silent -w '\n' --insecure -u elastic:elastic \
+echo 'Creating elasticsearch index.' >&3
+curl --fail-with-body --silent --show-error -w '\n' --insecure -u elastic:elastic \
     -XPUT "https://localhost:9200/wikiquote" \
     -H "Content-Type: application/json" \
     -d @index_settings.json
 # fail if could not create index
 if [ $? -ne 0 ]; then
-    echo 'failed to create elasticsearch index'
+    echo "Failed to create elasticsearch index, see $log_file" >&3
     exit 1
 fi
 
 
 # use the bulk api to load each chunk into elastic
-file_count=$(ls chunks -1q | wc -l)
+echo -en "Loading chunk(s) into elasticsearch" >&3
 i=1
 for file_name in chunks/*; do
-    echo "loading chunk: ${i}/${file_count}"
-    curl --fail-with-body --insecure -u elastic:elastic \
-        --silent --output /dev/null --show-error \
+    echo -en "\rLoading chunk(s) into elasticsearch: ${i}/${chunk_count}" >&3
+    curl --silent --show-error --fail-with-body -w '\n' \
+        --insecure -u elastic:elastic \
         -H "Content-Type: application/x-ndjson" \
         -XPOST 'https://localhost:9200/wikiquote/_bulk' \
-        --data-binary @"${file_name}"
+        --data-binary @"${file_name}" >> ingest.log
     i=$((i + 1))
 done
 # flush to ensure all elastic writes all data to index
+echo -e '\nFlushing elasticsearch index.' >&3
 curl --fail-with-body --silent --insecure -u elastic:elastic \
     -X POST "https://localhost:9200/wikiquote/_flush?pretty"
 
 
 # remove the chunks and original dump now that they've been loaded into elastic
-echo 'removing dump file and chunks'
+echo 'Removing wikimedia dump and chunk files.' >&3
 rm -r chunks
 rm $raw_file
 
 
-echo 'ingest complete'
+echo 'Ingest complete.' >&3

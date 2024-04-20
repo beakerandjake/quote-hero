00e32b4506e133e2e181e04dd2471bc6aaf90e06
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-20 15:51:55 -0700

    post search terms

--- api/src/routes/search.py
+++ api/src/routes/search.py
@@ -1,9 +1,20 @@
 from fastapi import APIRouter
+from pydantic import BaseModel, Field, validator
+
 
 router = APIRouter()
 
 
+class Query(BaseModel):
+    terms: list[str] = Field(min_items=1)
+
+    class Config:
+        str_strip_whitespace = True
+        str_min_length = 1
+        str_max_length = 50
+
+
 @router.post("/search")
-async def search():
+async def search(query: Query):
     """Searches for pages which match the given words"""
-    return "Search it!"
+    return f"You searched for: {query.terms}"

7b141246c4c43d5e970bcdd7e307a34dd4126bcf
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-20 12:05:28 -0700

    add script to return random word

---
+++ api/random_word.py
@@ -0,0 +1,19 @@
+import os
+from random import randrange
+
+# expect a words file to exist in our current dir.
+words_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "words.txt")
+
+
+def random_word():
+    """Returns a random words from the words file"""
+    result = None
+    with open(words_file_path) as words_file:
+        for i, word in enumerate(words_file, start=1):
+            if randrange(i) == 0:
+                result = word.strip()
+    return result
+
+
+if __name__ == "__main__":
+    print(random_word())

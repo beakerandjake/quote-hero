25a5e81c7af38dfe5f1dcce16c04147be9cc549b
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-21 16:40:56 -0700

    add page heading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from "react";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Hello World!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
+    <div className="min-h-full">
+      <main className="mx-auto max-w-7xl px-4 py-20 sm:px-6 md:py-24 lg:px-8">
+        {/* Title */}
+        <div className="text-center">
+          <h1 className="text-4xl font-bold lg:text-6xl">
+            QuoteMaster <span className="pl-1">💬</span>
+          </h1>
+          <div className="text-md mt-3 text-gray-500 md:text-xl  dark:text-slate-400">
+            How many pages can you match?
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
 

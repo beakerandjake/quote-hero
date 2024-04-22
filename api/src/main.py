35f87be0d4e18c660f409bfc4b660d6ee36bd347
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-22 11:34:09 -0700

    add cors

--- api/src/main.py
+++ api/src/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routes import words, search, stats
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
+)
+
 app.include_router(words.router)
 app.include_router(search.router)
 app.include_router(stats.router)

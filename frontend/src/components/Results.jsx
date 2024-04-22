f637b9f96de46ddcd402a6fee861c8d45b6eadc2
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-21 19:28:26 -0700

    link to pages

--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -1,10 +1,18 @@
 export const Results = ({ results }) => (
-  <div>
-    <h3 className="text-lg font-bold">Got {results.total} results</h3>
-    <ul className="mt-2 list-disc">
+  <div className="flex flex-col items-center gap-5">
+    <h3 className="text-lg font-bold">
+      You matched {results.total} results!!!
+    </h3>
+    <h4 className="text-md font-bold">Top Results:</h4>
+    <ul className="list-disc">
       {results.top.map((r, i) => (
         <li key={i}>
-          <a>{r.title}</a>
+          <a
+            className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+            href={`https://en.wikiquote.org/?curid=${r.pageId}`}
+          >
+            {r.title}
+          </a>
         </li>
       ))}
     </ul>

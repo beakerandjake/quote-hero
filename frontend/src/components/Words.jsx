f6f7cdf1eae5f1c11a7a5d37c858b5d5a0d6607b
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-22 14:36:23 -0700

    add empty display to words

--- frontend/src/components/Words.jsx
+++ frontend/src/components/Words.jsx
@@ -1,12 +1,37 @@
 import { Word } from "./Word";
 
 /**
- * Displays the words
+ * Container for words display
  */
-export const Words = ({ words }) => (
-  <div className="flex flex-wrap justify-center gap-3">
-    {words.map((word, i) => (
-      <Word word={word} key={i} />
-    ))}
+const Card = ({ children }) => (
+  <div className="relative inline-flex min-w-64 flex-wrap justify-center gap-3 rounded-lg border border-slate-300 p-5 shadow dark:border-gray-700">
+    {children}
   </div>
 );
+
+/**
+ * Component rendered when no words have been selected.
+ */
+const NoWords = () => (
+  <Card>
+    <div className="text-center dark:text-white">
+      <p>Add a word to get started.</p>
+    </div>
+  </Card>
+);
+
+/**
+ * Displays the words
+ */
+export const Words = ({ words }) => {
+  if (!words.length) {
+    return <NoWords />;
+  }
+  return (
+    <Card>
+      {words.map((word, i) => (
+        <Word word={word} key={i} />
+      ))}
+    </Card>
+  );
+};

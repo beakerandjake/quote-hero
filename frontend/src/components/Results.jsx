72fb0c16f83830431d13b3582a9c91134a3847f8
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-22 18:00:03 -0700

    results display

--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -1,9 +1,15 @@
+/**
+ * Container to display the results in.
+ */
 const Card = ({ children }) => (
-  <div className="rounded-lg border border-gray-200 px-8 py-5 shadow dark:border-gray-700">
+  <div className="rounded-lg border border-slate-300 px-8 py-5 shadow dark:border-gray-700">
     {children}
   </div>
 );
 
+/**
+ * Indicates that no results were found.
+ */
 const NoMatches = () => (
   <Card>
     <h1 className="text-md font-medium leading-6 text-gray-900  dark:text-white">
@@ -12,6 +18,9 @@ const NoMatches = () => (
   </Card>
 );
 
+/**
+ * Displays the results of the search
+ */
 export const Results = ({ results }) => {
   if (!results.total) {
     return <NoMatches />;
@@ -19,18 +28,22 @@ export const Results = ({ results }) => {
   return (
     <Card>
       {/* Header */}
-      <div className="flex flex-col gap-3">
-        <h1 className="text-xl font-medium leading-6 text-gray-900  dark:text-white">
+      <div className="mb-2">
+        <h1 className="text-2xl font-medium leading-6 text-gray-900 dark:text-white">
           {results.total === 1
             ? "Found One Match"
-            : `Found ${results.total} Matches`}
+            : `Found ${results.total.toLocaleString()} Matches`}
         </h1>
       </div>
-      <h3 className="mt-1 text-gray-500 dark:text-slate-400">Top Results:</h3>
-      <ul role="list" className="divide-y divide-gray-100">
+      {/* Content */}
+      <h3 className="mb-1 text-lg text-gray-500 dark:text-slate-400">
+        Top Results
+      </h3>
+      {/* List of links to the top pages */}
+      <ul role="list">
         {results.top.map(({ pageId, title }) => (
           <li
-            className="list-decimal font-medium text-blue-600 hover:underline dark:text-blue-500"
+            className="font-medium text-blue-600 hover:underline dark:text-blue-500"
             key={pageId}
           >
             <a href={`https://en.wikiquote.org/?curid=${pageId}`}>{title}</a>

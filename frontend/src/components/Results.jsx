fbfc519520285d016c754841ee37371620f0fc96
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-22 17:26:46 -0700

    wip results

--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -1,20 +1,42 @@
-export const Results = ({ results }) => (
-  <div className="flex flex-col items-center gap-5">
-    <h3 className="text-lg font-bold">
-      You matched {results.total} results!!!
-    </h3>
-    <h4 className="text-md font-bold">Top Results:</h4>
-    <ul className="list-disc">
-      {results.top.map((r, i) => (
-        <li key={i}>
-          <a
-            className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-            href={`https://en.wikiquote.org/?curid=${r.pageId}`}
-          >
-            {r.title}
-          </a>
-        </li>
-      ))}
-    </ul>
+const Card = ({ children }) => (
+  <div className="rounded-lg border border-gray-200 px-8 py-5 shadow dark:border-gray-700">
+    {children}
   </div>
 );
+
+const NoMatches = () => (
+  <Card>
+    <h1 className="text-md font-medium leading-6 text-gray-900  dark:text-white">
+      No Matches Found
+    </h1>
+  </Card>
+);
+
+export const Results = ({ results }) => {
+  if (!results.total) {
+    return <NoMatches />;
+  }
+  return (
+    <Card>
+      {/* Header */}
+      <div className="flex flex-col gap-3">
+        <h1 className="text-xl font-medium leading-6 text-gray-900  dark:text-white">
+          {results.total === 1
+            ? "Found One Match"
+            : `Found ${results.total} Matches`}
+        </h1>
+      </div>
+      <h3 className="mt-1 text-gray-500 dark:text-slate-400">Top Results:</h3>
+      <ul role="list" className="divide-y divide-gray-100">
+        {results.top.map(({ pageId, title }) => (
+          <li
+            className="list-decimal font-medium text-blue-600 hover:underline dark:text-blue-500"
+            key={pageId}
+          >
+            <a href={`https://en.wikiquote.org/?curid=${pageId}`}>{title}</a>
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};

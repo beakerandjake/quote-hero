5b38cbc0609609eb8c7205fcf144368c1979ae02
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-25 14:34:16 -0700

    update title

--- frontend/src/components/PageHeader.jsx
+++ frontend/src/components/PageHeader.jsx
@@ -4,7 +4,7 @@
 export const PageHeader = () => (
   <div className="text-center">
     <h1 className="text-4xl font-bold leading-tight tracking-tight text-gray-900 lg:text-6xl dark:text-white">
-      QuoteMaster <span className="pl-1">💬</span>
+      quote-master <span className="pl-1">💬</span>
     </h1>
     <div className="text-md mt-3 text-gray-500 md:text-xl  dark:text-slate-400">
       How many pages can you match?
